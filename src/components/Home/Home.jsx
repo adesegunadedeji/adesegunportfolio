b62129954601472d2ee5fe92487a456ba138f3b8
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-01-03 21:28:05 -0500

    creatded Navbar and Body(paragraph)

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+function HomeComponent (){
+        return (
+         <div className="homepage">
+            <p>A software developer - passionate about technology and sports and how it can impact our human experience</p>
+         </div>
+        )
+    
+}
+export default HomeComponent
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+function NavbarComponent (){
+        return (
+<div className="NavbarComponent">
+  <header className="active-navbar">
+    <div className="container logo-left-menu-right">
+	    <div className="logo navbar-left">
+       <a href="https://www.adeadedeji.com"className="a-Logo" title="Ade Adedeji">ade adedeji</a>
+      </div>
+      <nav className="navbar-right">
+        <ul className="menu"> 
+        <li className="menu-item"><a className="menu-Logo"  href="https://www.adeadedeji.com/about"><span>About</span></a></li>
+        <li className="menu-item"><a className="menu-Logo"  href="https://www.adeadedeji.com/projects"><span>Projects</span></a></li>
+        <li className="menu-item"><a className="menu-Logo" href="mailto:adesegunadedeji@gmail.com"><span>Contact</span></a></li>
+        </ul></nav>
+        <div class="hamburger navbar-right semplice-menu"><a class="open-menu menu-icon"><span></span></a></div>
+    </div>
+  </header>
+</div>)
+}
+export default NavbarComponent
