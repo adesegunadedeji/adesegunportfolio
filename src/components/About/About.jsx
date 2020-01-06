d408ed5bb3240c91acbe716a9bc83eb0ec36b9d1
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-01-05 20:20:41 -0500

    made changes to About.css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,29 +4,36 @@ import './About.css';
 function About() {
     return (
         <div className="aboutPage">
-            <h1 className = "header1About"> Hi, I'm Ade. Nice to meet you</h1>
-            <span><p>I believe in creating product that can change the literally change the world and increase our global awareness. My skills are broad: from front end to back end development.I enjoy building sites  and mobile applications from start to finish.</p></span>
+            <h1 className = "header1About"> Hi, I'm Ade...</h1>
+            <span><p className="aboutParagraph">I believe in creating product that can change the literally change the world and increase our global awareness. My skills are broad: from front end to back end development.I enjoy building sites  and mobile applications from start to finish.</p></span>
 
-            <span><p>I currently spend my days as a design Engineer at PSEG. In my evenings, I'm finding ways to turn thoughtful ideas into responsive websites that allow the user experience the application in the best possible way. On weekends, I cook and enjoy watching the English Premier League.</p></span>
-            <span><p>
+            <span><p className="aboutParagraph">I currently spend my days as a design Engineer at PSEG. In my evenings, I'm finding ways to turn thoughtful ideas into responsive websites that allow the user experience the application in the best possible way. On weekends, I cook and enjoy watching the English Premier League.</p></span>
+            <span><p className="aboutParagraph">
                 I've recently relocated to Seattle, Washington after spending the last 10 years living across the East Coast. Before that, I called Lagos, Nigeria home.</p></span>
             
-            <span><p>I'm grateful to have had the opportunity to work in Engineering at PSEG, IBM and Exelon Corporation. Prior, I attended Drexel University at Philadelphia, PA where I completed my Master's degree in Electrical and Computer Engineering and my Bachelor's Degree at Morgan State University in Baltimore, MD where I also studied Electrical and Computer Engineering. </p></span>
+            <span><p className="aboutParagraph">I'm grateful to have had the opportunity to work in Engineering at PSEG, IBM and Exelon Corporation. Prior, I attended Drexel University at Philadelphia, PA where I completed my Master's degree in Electrical and Computer Engineering and my Bachelor's Degree at Morgan State University in Baltimore, MD where I also studied Electrical and Computer Engineering.
+    </p></span>
+            <span><p className="aboutParagraph">
+            I'm available for remote work - if you would like to build something together, <a href="mailto:adesegunadedeji@gmail.com">get in touch?</a>
+             </p></span>
             <h2 className = "header2About">What I do</h2>
             <div  className="unorderlist">
-            <ul  className="ULabout"> Front-end
+            <ul  className="ULabout"> 
+            <h7 className="h7About"> Front-end</h7>
                 <li>CSS3</li>
                 <li>Javascript</li>
                 <li>ES6</li>
                 <li>React</li>
             </ul>
-            <ul className="ULabout"> Back-end
+            <ul className="ULabout"> 
+            <h7 className="h7About"> Back-end</h7>
                 <li>NodeJS</li>
                 <li> Ruby</li>
                  <li>Ruby on Rails</li>
                  <li> APIs</li>
             </ul>
-            <ul className="ULabout"> Cloud
+            <ul className="ULabout"> 
+            <h7 className="h7About">Cloud</h7>
                 <li>AWS</li>
                 <li> Google</li>
                 <li>Firebase</li>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,20 @@ function Footer (){
     return(
         <div className="footer">
             <div className="column1"> 
-            <h7>NOW</h7>
+            <h7 className="h7Footer">NOW</h7>
             <ul className="menuFooter">
             <li className="Footer-item"><span className="Bullet">PSEG</span></li> 
             </ul>
             </div>
             <div className="column2"> 
-            <h7> PREVIOUS</h7>
+            <h7 className="h7Footer"> PREVIOUS</h7>
             <ul className="menuFooter">
             <li className="Footer-item"><span className="Bullet">IBM</span></li> 
             <li className="Footer-item"><span className="Bullet"> EXELON</span></li> 
             </ul>
             </div>
             <div className="column3">
-                <h7>CONNECT</h7> 
+                <h7 className="h7Footer">CONNECT</h7> 
             <ul className="menuFooter">
             <li className="Footer-item"><a className="a-footer" href="https://twitter.com/Ade_Segun"><span>Twitter</span></a></li>
             <li className="Footer-item"><a  className="a-footer" href="https://medium.com/@adesegunadedeji"><span>Medium</span></a></li>
